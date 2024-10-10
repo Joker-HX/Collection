5aba506f0cbbe41ddb5aaa95f48e2001e1c9608c
Joker.W <60083151+Joker-HX@users.noreply.github.com>
2024-10-10 21:33:55 +0800

    Update Waterlytics.js

--- script/Waterlytics.js
+++ script/Waterlytics.js
@@ -3,7 +3,7 @@
 喝水时间 - 提醒：https://apps.apple.com/app/id1401162094
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/Waterlytics.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Joker-HX/Collection/refs/heads/main/script/Waterlytics.js
 
 [MITM]
 hostname = api.revenuecat.com
